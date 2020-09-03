08186381a2802d7e41cf3fa365c4172190bfc781
Andre King <andreking1031@gmail.com>
2020-09-03 17:36:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  def initialize(url)
+    get_requester = GetRequester.new(URL)
+    
+  end
+  
+  def get_response_body
+    
+  end
+  
+  def parse_json
+    
+  end 
+end
